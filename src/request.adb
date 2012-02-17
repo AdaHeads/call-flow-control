0032f031e3066d5c0c097dcfe2e7027b79d5cf7f
Thomas Løcke <tl@adaheads.com>
2012-02-17 15:09:45 +0100

    Minor tweaks and fixes to the Call_Queue package.

--- src/request.adb
+++ src/request.adb
@@ -125,36 +125,18 @@ package body Request is
      (Request : in AWS.Status.Data)
       return AWS.Response.Data
    is
-      --  use Ada.Calendar;
-      --  use AWS.Utils;
-   begin
---        Call_Queue.Add (Id         => Random_String (10),
---                        Callee     => "88245300",
---                        Caller     => "+45 60431990",
---                        Priority   => Call_Queue.Low,
---                        Start      => Clock);
---
---        Call_Queue.Add (Id         => Random_String (10),
---                        Callee     => "33488200",
---                        Caller     => "+45 60431992",
---                        Priority   => Call_Queue.Normal,
---                        Start      => Clock + 1.0);
---
---        Call_Queue.Add (Id         => Random_String (10),
---                        Callee     => "88329100",
---                        Caller     => "60431993",
---                        Priority   => Call_Queue.High,
---                        Start      => Clock + 2.0);
---
---        Call_Queue.Add (Id         => "abcdefghij",
---                        Callee     => "88329100",
---                        Caller     => "60431993",
---                        Priority   => Call_Queue.High,
---                        Start      => Clock + 2.0);
+      use AWS.Status;
 
-      return Build_Response
-        (Status_Data => Request,
-         Content     => Call_Queue.Get);
+      P : constant AWS.Parameters.List := Parameters (Request);
+   begin
+      if P.Get ("kind") = "length" then
+         return Build_Response (Status_Data => Request,
+                                Content     => Call_Queue.Length);
+      else
+         return Build_Response
+           (Status_Data => Request,
+            Content     => Call_Queue.Get);
+      end if;
    end Queue;
 
 end Request;
