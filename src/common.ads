a9b6609bcc5496927521c153df2cbfc3d3559de9
Thomas Løcke <tl@adaheads.com>
2012-05-22 10:24:01 +0200

    Added the Common package.
    Contains stuff common to a lot of Alice packages.

---
+++ src/common.ads
@@ -0,0 +1,38 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                                  Alice                                    --
+--                                                                           --
+--                                 Common                                    --
+--                                                                           --
+--                                  SPEC                                     --
+--                                                                           --
+--                     Copyright (C) 2012-, AdaHeads K/S                      --
+--                                                                           --
+--  This is free software;  you can redistribute it and/or modify it         --
+--  under terms of the  GNU General Public License  as published by the      --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any  --
+--  later version. This library is distributed in the hope that it will be   --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--  You should have received a copy of the GNU General Public License and    --
+--  a copy of the GCC Runtime Library Exception along with this program;     --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+with Ada.Strings.Bounded;
+
+package Common is
+
+   use Ada.Strings;
+
+   package JSON_Large is new Bounded.Generic_Bounded_Length (10_000);
+   --  Used to hold JSON strings which are considered "large", ie. collections
+   --  of contacts, attributes or similar.
+
+   package JSON_Small is new Bounded.Generic_Bounded_Length (1_000);
+   --  Used to hold JSON strings which are considered "small", ie. a single
+   --  contact, organization or similar.
+
+end Common;
