e9de0765f9009739b9c46cddb264db1ce4202e89
Kim Rostgaard Christensen <krc@retrospekt.dk>
2012-12-20 23:03:30 +0100

    Added a few more actions. Still a long way to go... :-(

--- src/pbx/ami-client.adb
+++ src/pbx/ami-client.adb
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--                                                                           --
+--                                                                           --
 --                     Copyright (C) 2012-, AdaHeads K/S                     --
 --                                                                           --
 --  This is free software;  you can redistribute it and/or modify it         --
