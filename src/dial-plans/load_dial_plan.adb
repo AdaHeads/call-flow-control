e389d4d1cc7f22447f35153fd855807891b734b6
Jacob Sparre Andersen <jsa@adaheads.com>
2013-02-14 14:16:54 +0100

    Creating (a still mostly empty) dial-plan object.

--- src/dial-plans/load_dial_plan.adb
+++ src/dial-plans/load_dial_plan.adb
@@ -26,9 +26,14 @@ with DOM.Core;                 use DOM.Core;
 with DOM.Core.Documents;       use DOM.Core.Documents;
 with DOM.Core.Nodes;           use DOM.Core.Nodes;
 
-with Receptions.End_Points.Queue;
+with Receptions.Decision_Tree_Collection,
+     Receptions.Dial_Plan,
+     Receptions.End_Point_Collection,
+     Receptions.End_Points.Queue;
 
 procedure Load_Dial_Plan is
+   procedure Check (Element : in     Node;
+                    Name    : in     String);
    procedure Check (Element : in     Node;
                     Name    : in     String) is
    begin
@@ -45,12 +50,16 @@ procedure Load_Dial_Plan is
    Reader : Tree_Reader;
    Doc    : Document;
 
+   Reception : Receptions.Dial_Plan.Instance;
+
    Dial_Plan_Title    : Ada.Strings.Unbounded.Unbounded_String;
    Start_Action_Title : Ada.Strings.Unbounded.Unbounded_String;
+   End_Points         : Receptions.End_Point_Collection.Map;
+   Decision_Trees     : Receptions.Decision_Tree_Collection.Map;
 begin
    Set_Public_ID (Input, "dial-plan");
 
-   Open_Source_File:
+   Open_Source_File :
    declare
       use Ada.Command_Line, Ada.Text_IO;
    begin
@@ -74,16 +83,18 @@ begin
    Parse (Reader, Input);
    Close (Input);
 
-   Doc := Get_Tree (Reader); 
+   Doc := Get_Tree (Reader);
 
    declare
-      Dial_Plan : Node := Get_Element (Doc);
+      Dial_Plan : constant Node := Get_Element (Doc);
    begin
       Check (Element => Dial_Plan, Name => "dial-plan");
 
       declare
          use Ada.Strings.Unbounded;
-         Title_Attribute : not null Node := Get_Named_Item (Nodes.Attributes (Dial_Plan), "title");
+         Title_Attribute : constant not null Node :=
+                             Get_Named_Item (Nodes.Attributes (Dial_Plan),
+                                             "title");
       begin
          Dial_Plan_Title := To_Unbounded_String (Node_Value (Title_Attribute));
       end;
@@ -101,18 +112,20 @@ begin
 
          declare
             use Ada.Strings.Unbounded;
-            Do_Attribute : not null Node := Get_Named_Item (Nodes.Attributes (Start), "do");
+            Do_Attribute : constant not null Node :=
+                             Get_Named_Item (Nodes.Attributes (Start), "do");
          begin
-            Start_Action_Title := To_Unbounded_String (Node_Value (Do_Attribute));
+            Start_Action_Title :=
+              To_Unbounded_String (Node_Value (Do_Attribute));
          end;
       end;
 
       declare
          End_Point : Node := First_Child (Dial_Plan);
       begin
-         Find_End_Points:
+         Find_End_Points :
          loop
-            Next_End_Point:
+            Next_End_Point :
             loop
                exit Find_End_Points when End_Point = null;
                exit Next_End_Point when Node_Type (End_Point) = Element_Node and then
@@ -124,7 +137,8 @@ begin
 
             declare
                use Ada.Strings.Unbounded, Ada.Text_IO;
-               Title_Attribute : not null Node := Get_Named_Item (Nodes.Attributes (End_Point), "title");
+               Title_Attribute : constant not null Node :=
+                                   Get_Named_Item (Nodes.Attributes (End_Point), "title");
                Title           : constant String := Node_Value (Title_Attribute);
             begin
                declare
@@ -144,12 +158,15 @@ begin
                      declare
                         use Receptions.End_Points.Queue;
 
-                        ID_Attribute : not null Node := Get_Named_Item (Nodes.Attributes (End_Point_Action),
+                        ID_Attribute : constant not null Node :=
+                                         Get_Named_Item (Nodes.Attributes (End_Point_Action),
                                                                         "id");
                         Queue : Receptions.End_Points.Queue.Instance;
                      begin
                         Queue := Create (Title => Title,
                                          ID    => Node_Value (ID_Attribute));
+                        End_Points.Insert (Key      => Title,
+                                           New_Item => Queue);
                      end;
                   elsif Node_Name (End_Point_Action) = "redirect" then
                      Put_Line ("End-point type:        redirect");
@@ -174,9 +191,19 @@ begin
    end;
 
    declare
+      use Ada.Strings.Unbounded;
+   begin
+      Reception := Receptions.Dial_Plan.Create
+                     (Title          => To_String (Dial_Plan_Title),
+                      Start_At       => To_String (Start_Action_Title),
+                      End_Points     => End_Points,
+                      Decision_Trees => Decision_Trees);
+   end;
+
+   declare
       use Ada.Strings.Unbounded, Ada.Text_IO;
    begin
-      Put_Line ("Dial-plan title:       """ & To_String (Dial_Plan_Title) & """");
+      Put_Line ("Dial-plan title:       """ & Reception.Title & """");
       Put_Line ("Title of first action: """ & To_String (Start_Action_Title) & """");
    end;
 end Load_Dial_Plan;
