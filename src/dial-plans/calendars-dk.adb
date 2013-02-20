3e1bc9264d1edcd685d806d30629f792cd58ee6a
Jacob Sparre Andersen <jsa@adaheads.com>
2013-02-20 12:12:19 +0100

    Including holidays relative to Easter.  (Consider if the Easter calculation should be made public or moved to Calendars.Gregorian.)

--- src/dial-plans/calendars-dk.adb
+++ src/dial-plans/calendars-dk.adb
@@ -15,22 +15,71 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
-with Ada.Calendar.Formatting;
+with Ada.Calendar.Arithmetic,
+     Ada.Calendar.Formatting;
 
 with System_Message.Critical;
 
 package body Calendars.DK is
+   procedure Easter (Year  : in     Ada.Calendar.Year_Number;
+                     Month :    out Ada.Calendar.Month_Number;
+                     Day   :    out Ada.Calendar.Day_Number)
+     with Precondition => (Year in 1900 .. 2099);
+
+   procedure Easter (Year  : in     Ada.Calendar.Year_Number;
+                     Month :    out Ada.Calendar.Month_Number;
+                     Day   :    out Ada.Calendar.Day_Number) is
+      H, I, J, L : Integer;
+   begin
+      H := (24 + 19 * (Year mod 19)) mod 30;
+      I := H - (H / 28);
+      J := (Year + (Year / 4) + I - 13) mod 7;
+      L := I - J;
+      Month := 3 + ((L + 40) / 44);
+      Day   := L + 28 - 31 * (Month / 4);
+   end Easter;
+
+   function Easter
+     (Date   : in Ada.Calendar.Time;
+      Offset : in Ada.Calendar.Arithmetic.Day_Count) return Boolean;
+
+   function Easter
+     (Date   : in Ada.Calendar.Time;
+      Offset : in Ada.Calendar.Arithmetic.Day_Count) return Boolean is
+      use Ada.Calendar.Arithmetic;
+      Year  : constant Ada.Calendar.Year_Number  := Ada.Calendar.Year
+                                                      (Date - Offset);
+      Month : constant Ada.Calendar.Month_Number := Ada.Calendar.Month
+                                                      (Date - Offset);
+      Day   : constant Ada.Calendar.Day_Number   := Ada.Calendar.Day
+                                                      (Date - Offset);
+      Easter_Month : Ada.Calendar.Month_Number;
+      Easter_Day   : Ada.Calendar.Day_Number;
+   begin
+      Easter (Year  => Year,
+              Month => Easter_Month,
+              Day   => Easter_Day);
+      return Month = Easter_Month and then Day = Easter_Day;
+   end Easter;
+
    function Official_Holiday (Date : in Ada.Calendar.Time) return Boolean is
       Year  : constant Ada.Calendar.Year_Number  := Ada.Calendar.Year  (Date);
       Month : constant Ada.Calendar.Month_Number := Ada.Calendar.Month (Date);
       Day   : constant Ada.Calendar.Day_Number   := Ada.Calendar.Day   (Date);
       use Ada.Calendar.Formatting;
+      use type Ada.Calendar.Arithmetic.Day_Count;
    begin
       System_Message.Critical.Partial_Calendar_Data;
 
       return
         (Day_Of_Week (Date) = Sunday) or else -- Sunday
         (Month = 1  and then Day = 1) or else -- New Year
+        (Easter (Date,  -3)) or else --  Skærtorsdag
+        (Easter (Date,  -2)) or else --  Langfredag
+        (Easter (Date,  +1)) or else --  Anden påskedag
+        (Easter (Date, +26)) or else --  Store bededag
+        (Easter (Date, +39)) or else --  Kristi himmelfartsdag
+        (Easter (Date, +50)) or else --  Anden pinsedag
         (Month = 12 and then Day in 25 .. 26); --  Christmas
    end Official_Holiday;
 
