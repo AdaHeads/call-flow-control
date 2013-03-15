a1ca0fa6e758de572b4b5d29e71b04236a9d60d6
Jacob Sparre Andersen <jsa@adaheads.com>
2013-03-15 15:39:52 +0100

    Reordered functions.  Adjust exeption message, when an exception is raised inside Create.

--- src/dial-plans/receptions-decision_tree.adb
+++ src/dial-plans/receptions-decision_tree.adb
@@ -15,22 +15,9 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
+with Ada.Exceptions;
+
 package body Receptions.Decision_Tree is
-   function Create (Title     : in     String;
-                    Branches  : in     Receptions.List_Of_Branches.Vector;
-                    Fall_Back : in     String) return Instance is
-   begin
-      return
-        (Title     => Ada.Strings.Unbounded.To_Unbounded_String (Title),
-         Branches  => Branches,
-         Fall_Back => Ada.Strings.Unbounded.To_Unbounded_String (Fall_Back));
-   end Create;
-
-   function Title (Item : in     Instance) return String is
-   begin
-      return Ada.Strings.Unbounded.To_String (Item.Title);
-   end Title;
-
    function Branch (Item : in     Instance;
                     Call : in     PBX.Call.Identification) return String is
    begin
@@ -42,4 +29,23 @@ package body Receptions.Decision_Tree is
 
       return Ada.Strings.Unbounded.To_String (Item.Fall_Back);
    end Branch;
+
+   function Create (Title     : in     String;
+                    Branches  : in     Receptions.List_Of_Branches.Vector;
+                    Fall_Back : in     String) return Instance is
+   begin
+      return
+        (Title     => Ada.Strings.Unbounded.To_Unbounded_String (Title),
+         Branches  => Branches,
+         Fall_Back => Ada.Strings.Unbounded.To_Unbounded_String (Fall_Back));
+   exception
+      when E : Constraint_Error =>
+         raise Constraint_Error with "Receptions.Decision_Tree.Create: " &
+                                     Ada.Exceptions.Exception_Message (E);
+   end Create;
+
+   function Title (Item : in     Instance) return String is
+   begin
+      return Ada.Strings.Unbounded.To_String (Item.Title);
+   end Title;
 end Receptions.Decision_Tree;
