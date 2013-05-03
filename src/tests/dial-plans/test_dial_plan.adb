e4364e1195629f11a2d8c1ee9f6e2d3a3bc42f36
Jacob Sparre Andersen <jsa@adaheads.com>
2013-05-03 13:04:23 +0000

    The dial-plan tester now makes a look-up for what to do with a call to +45 21 49 08 04. (#66)

--- src/tests/dial-plans/test_dial_plan.adb
+++ src/tests/dial-plans/test_dial_plan.adb
@@ -18,38 +18,51 @@
 with Ada.Command_Line,
      Ada.Exceptions,
      Ada.Text_IO;
-with Receptions.No_PBX;
+with Receptions.No_PBX,
+     Yolk.Command_Line;
 with Model.Dial_Plans;
 
 procedure Test_Dial_Plan is
    use Ada.Command_Line, Ada.Exceptions, Ada.Text_IO;
 begin
-   Model.Dial_Plans.Container.Reload_All;
-   
    Set_Exit_Status (Success);
-   for Index in 1 .. Argument_Count loop
-      declare
-	 Number : String renames Argument (Index);
+
+   Loading_Dial_Plans_From_Database :
+   begin
+      Model.Dial_Plans.Container.Reload_All;
+   exception
+      when E : others =>
+         Set_Exit_Status (Failure);
+         Put_Line
+           (File => Standard_Error,
+            Item => "Test_Dial_Plan terminated by an unexpected exception " &
+                    "while loading dial-plans from the database: " &
+                    Exception_Name (E) & " (" & Exception_Message (E) & ")");
+         return;
+   end Loading_Dial_Plans_From_Database;
+
+   for Number of Yolk.Command_Line.Get ("--phone-number") loop
       begin
-	 Put_Line (Item => """" & Number & """ goes to the end-point """ & 
-		           Model.Dial_Plans.Container.End_Point
-		             (Number => Number,
-			      Call   => Receptions.No_PBX.Null_Call).Value);
+         Put_Line (Item => """" & Number & """ goes to the end-point """ &
+                           Model.Dial_Plans.Container.End_Point
+                             (Number => Number,
+                              Call   => Receptions.No_PBX.Null_Call).Value);
       exception
-	 when E : others =>
-	    Set_Exit_Status (Failure);
-	    Put_Line
-	      (File => Standard_Error,
-	       Item => "Test_Dial_Plan terminated by an unexpected exception " &
-		       "while testing the phone number """ & Number & """: " &
-                       Exception_Name (E) & " (" & Exception_Message (E) & ")");
+         when E : others =>
+            Set_Exit_Status (Failure);
+            Put_Line
+              (File => Standard_Error,
+               Item => "Test_Dial_Plan terminated by an unexpected " &
+                       "exception while testing the phone number """ & Number &
+                       """: " & Exception_Name (E) & " (" &
+                       Exception_Message (E) & ")");
       end;
    end loop;
 exception
    when E : others =>
       Set_Exit_Status (Failure);
       Put_Line
-	(File => Standard_Error,
-	 Item => "Test_Dial_Plan terminated by an unexpected exception: " &
-	         Exception_Name (E) & " (" & Exception_Message (E) & ")");
+        (File => Standard_Error,
+         Item => "Test_Dial_Plan terminated by an unexpected exception: " &
+                 Exception_Name (E) & " (" & Exception_Message (E) & ")");
 end Test_Dial_Plan;
