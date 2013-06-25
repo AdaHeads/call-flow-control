fefc37a6f47bae69a81e2e7d5b9ea78ccb0e7f41
Jacob Sparre Andersen <jsa@adaheads.com>
2013-06-25 13:22:38 +0000

    Database queries to look up users and their OpenIDs.

---
+++ src/model/sql/sql_prepared_statements-users.ads
@@ -0,0 +1,39 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                     Copyright (C) 2013-, AdaHeads K/S                     --
+--                                                                           --
+--  This is free software;  you can redistribute it and/or modify it         --
+--  under terms of the  GNU General Public License  as published by the      --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any  --
+--  later version. This library is distributed in the hope that it will be   --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--  You should have received a copy of the GNU General Public License and    --
+--  a copy of the GCC Runtime Library Exception along with this program;     --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+with GNATCOLL.SQL.Exec;
+
+with SQL_Prepared_Statements.Configuration,
+     SQL_Statements.Users;
+
+package SQL_Prepared_Statements.Users is
+
+   use GNATCOLL.SQL.Exec;
+
+   List : constant Prepared_Statement
+     := Prepare (Query         => SQL_Statements.Users.User_List_Query,
+                 Auto_Complete => True,
+                 On_Server     => Configuration.On_Server,
+                 Name          => "user_list");
+
+   OpenIDs : constant Prepared_Statement
+     := Prepare (Query         => SQL_Statements.Users.OpenID_List_Query,
+                 Auto_Complete => True,
+                 On_Server     => Configuration.On_Server,
+                 Name          => "openid_list");
+
+end SQL_Prepared_Statements.Users;
---
+++ src/model/sql/sql_statements-users.ads
@@ -0,0 +1,34 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                     Copyright (C) 2013-, AdaHeads K/S                     --
+--                                                                           --
+--  This is free software;  you can redistribute it and/or modify it         --
+--  under terms of the  GNU General Public License  as published by the      --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any  --
+--  later version. This library is distributed in the hope that it will be   --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--  You should have received a copy of the GNU General Public License and    --
+--  a copy of the GCC Runtime Library Exception along with this program;     --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+with GNATCOLL.SQL;
+
+package SQL_Statements.Users is
+
+   use GNATCOLL.SQL;
+
+   User_List_Query : constant SQL_Query
+     := SQL_Select (Fields => Distinct (DB.User_IDs.Name),
+                    From   => DB.User_IDs);
+
+   OpenID_List_Query : constant SQL_Query
+     := SQL_Select (Fields   => DB.User_IDs.OpenID,
+                    From     => DB.User_IDs,
+                    Where    => DB.User_IDs.Name = String_Param (1),
+                    Order_By => DB.User_IDs.Rank);
+
+end SQL_Statements.Users;
