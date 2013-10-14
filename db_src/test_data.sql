be61281547cc71b8ec135903caf4ac4e983da3a4
Jacob Sparre Andersen <jsa@adaheads.com>
2013-10-14 09:48:53 +0000

    Fixes to schema and test data.

--- db_src/test_data.sql
+++ db_src/test_data.sql
@@ -16,24 +16,24 @@ VALUES (1, 'AdaHeads K/S', 'adaheads_ks_1', '{"website":"http://adaheads.com","g
        (3, 'Responsum K/S', 'responsum_ks_3', '{"website":"http://responsum.dk","greeting":"Velkommen til Responsum - du taler med ..."}'),
        (4, 'Hansen VVS A/S', 'hansen_vvs_4', '{"website":"http://hansenvvs.dk","greeting":"Hansen VVS goddag"}');
 
-INSERT INTO organization_contacts (organization_id, contact_id, attributes, distribution_list_id)
+INSERT INTO organization_contacts (organization_id, contact_id, attributes)
 VALUES --  Adaheads
-       (1, 1, '{"tags":["AWS","Slackware"]}', 1),
-       (1, 2, '{"tags":["usability"]}', 1),
-       (1, 3, '{"tags":["CFO"]}', 2),
-       (1, 4, '{"tags":["Ada","Linux"]}', 2),
-       (1, 5, '{"tags":["Ada","physics"]}', 2),
-       (1, 6, '', 3),
-       (1, 7, '{"tags":["embedded"]}', 3),
+       (1, 1, '{"tags":["AWS","Slackware"]}'),
+       (1, 2, '{"tags":["usability"]}'),
+       (1, 3, '{"tags":["CFO"]}'),
+       (1, 4, '{"tags":["Ada","Linux"]}'),
+       (1, 5, '{"tags":["Ada","physics"]}'),
+       (1, 6, NULL),
+       (1, 7, '{"tags":["embedded"]}'),
        --  Fishermans Friends
-       (2, 1, '{"tags":["Ada","Slackware","Linux"]}', 4),
-       (2, 4, '{"tags":["Ada","Slackware","Linux"]}', 4),
-       (2, 8, '', 5),
+       (2, 1, '{"tags":["Ada","Slackware","Linux"]}'),
+       (2, 4, '{"tags":["Ada","Slackware","Linux"]}'),
+       (2, 8, NULL),
        --  Responsum
-       (3, 1, '}', 5),
-       (3, 2, '{"tags":["IT","Support","Printer"]}', 5),
-       (3, 3, '{"tags":["jobansøger","2730","3660","3520"]}', 5),
-       (3, 4, '{"tags":["Ny kunde","Salg","Uadresserede"]}', 5);
+       (3, 1, NULL),
+       (3, 2, '{"tags":["IT","Support","Printer"]}'),
+       (3, 3, '{"tags":["jobansøger","2730","3660","3520"]}'),
+       (3, 4, '{"tags":["Ny kunde","Salg","Uadresserede"]}');
 
 INSERT INTO messaging_addresses (id, address_type, address)
 VALUES (1, 'e-mail', 'tl@adaheads.com'),
@@ -51,26 +51,26 @@ INSERT INTO messaging_end_points (contact_id, organization_id,
 VALUES --  Adaheads
        (1, 1, 1, FALSE, TRUE),
        (2, 1, 2, FALSE, FALSE),
-       (3, 4, 3, FALSE, TRUE),
-       (4, 4, 4, FALSE, TRUE),
-       (5, 4, 5, TRUE,  FALSE),
+       (3, 1, 3, FALSE, TRUE),
+       (4, 1, 4, FALSE, TRUE),
+       (5, 1, 5, TRUE,  FALSE),
        --  Fishermans Friends                
        (6, 1, 6, FALSE, TRUE),
        (7, 1, 7, FALSE, FALSE),
        --  Responsum
-       (8, 1, 8, FALSE, TRUE),
-       (9, 2, 9, FALSE, TRUE);
+       (8, 2, 2, FALSE, TRUE),
+       (4, 3, 9, FALSE, TRUE);
 
 INSERT INTO distribution_lists (id,
-                                          send_to_contact_id, send_to_organization_id,
-                                          recipient_visibility)
+                                send_to_contact_id, send_to_organization_id,
+                                recipient_visibility)
 VALUES (1, 1, 1, 'to'),
        (2, 1, 2, 'cc'),
        (3, 1, 2, 'to'),
        (4, 1, 1, 'cc'),
-       (5, 1, 4, 'to'),
+       (5, 1, 3, 'to'),
        (6, 3, 1, 'to'),
-       (7, 3, 4, 'to');
+       (7, 3, 3, 'to');
 
 INSERT INTO kinds (id)
 VALUES ('helligdag');
@@ -81,7 +81,7 @@ VALUES ('helligdag', '2013-12-25'),
        ('helligdag', '2014-01-01');
 
 INSERT INTO dial_plans (phone_number, dial_plan)
-VALUES ('+4521490804', '<?xml version="1.0"?> <!DOCTYPE dial-plan> <dial-plan title="Jacob: Hang up on anonymous callers"> <start do="Start"/> <decision-tree title="Start"> <branch> <conditions> <caller number=""/> </conditions> <action do="Hang up"/> </branch> <fall-back do="Pass through"/> </decision-tree> <end-point title="Hang up"> <hang-up/> </end-point> <end-point title="Pass through"> <redirect to="+45 21 49 08 04"/> </end-point> </dial-plan>');
+VALUES ('+4521490804', '<dial-plan title="Jacob: Hang up on anonymous callers"> <start do="Start"/> <decision-tree title="Start"> <branch> <conditions> <caller number=""/> </conditions> <action do="Hang up"/> </branch> <fall-back do="Pass through"/> </decision-tree> <end-point title="Hang up"> <hang-up/> </end-point> <end-point title="Pass through"> <redirect to="+45 21 49 08 04"/> </end-point> </dial-plan>');
 
 INSERT INTO users (name, is_receptionist, is_service_agent, is_administrator)
 VALUES ('Tux',                      FALSE, FALSE, TRUE),
