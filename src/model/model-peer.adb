7e6ffe002422d54bec075451a17e1014cfe09e5c
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-01-01 22:05:17 +0100

    Added comments and expiry times to peers.

--- src/model/model-peer.adb
+++ src/model/model-peer.adb
@@ -15,7 +15,20 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
+with Ada.Calendar;
+
 package body Model.Peer is
+   use Ada.Calendar;
+
+   procedure Bump_Expiry (Object     :    out Instance;
+                          Time_Delta : in     Natural) is
+   begin
+      Object.Expiry_Time := Current_Time + Duration (Time_Delta);
+   end Bump_Expiry;
+
+   --------------
+   --  Create  --
+   --------------
 
    function Create (User_ID : Identification;
                     Values  : JSON_Value) return Instance is
@@ -25,29 +38,56 @@ package body Model.Peer is
          Is_Registered := True;
       end if;
 
-      return (User_ID    => User_ID,
-              Values     => Values,
-              Registered => Is_Registered);
+      return (User_ID     => User_ID,
+              Values      => Values,
+              Registered  => Is_Registered,
+              Expiry_Time => <>);
    end Create;
+
+   --------------------------
+   --  Get_Identification  --
+   --------------------------
 
    function Get_Identification (Object : in Instance) return String is
    begin
       return Object.Values.Get (Field => Extension_String);
    end Get_Identification;
 
+   ----------------
+   --  Register  --
+   ----------------
+
    procedure Register (Object : out Instance) is
    begin
       Object.Registered := True;
    end Register;
+
+   ------------------
+   --  Registered  --
+   ------------------
 
    function Registered (Object : in Instance) return Boolean is
    begin
       return Object.Registered;
    end Registered;
 
+   ---------------
+   --  To_JSON  --
+   ---------------
+
    function To_JSON (Object : in Instance) return JSON_Value is
-      Root : constant JSON_Value := Create_Object;
+      Root       : constant JSON_Value := Create_Object;
+      Expires_In : constant Duration :=
+        Object.Expiry_Time - Common.Current_Time;
    begin
+      if Expires_In < 0.0 then
+         Root.Set_Field (Field_Name => Expires_String,
+                         Field      => Integer'(0));
+      else
+         Root.Set_Field (Field_Name => Expires_String,
+                         Field      => Integer (Expires_In));
+      end if;
+
       Root.Set_Field (Field_Name => Registered_String,
                       Field      => Object.Registered);
       Root.Set_Field (Field_Name => To_String (Object.User_ID),
@@ -55,6 +95,9 @@ package body Model.Peer is
       return Root;
    end To_JSON;
 
+   ------------------
+   --  Unregister  --
+   ------------------
    procedure Unregister (Object : out Instance) is
    begin
       Object.Registered := False;
