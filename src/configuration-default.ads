eb65402fa5d5b14b1d06d5a99e1cf641b8a6441b
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-03-14 09:57:50 +0100

    Branched the configuration defaults to a seperate file.

---
+++ src/configuration-default.ads
@@ -0,0 +1,75 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                     Copyright (C) 2014-, AdaHeads K/S                     --
+--                                                                           --
+--  This is free software;  you can redistribute it and/or modify it         --
+--  under terms of the  GNU General Public License  as published by the      --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any  --
+--  later version. This library is distributed in the hope that it will be   --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--  You should have received a copy of the GNU General Public License and    --
+--  a copy of the GCC Runtime Library Exception along with this program;     --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+with Common;
+
+private package Configuration.Default is
+   use Ada.Strings.Unbounded;
+   use Common;
+
+   type Keys is (Cache_Max_Element_Age,
+                 Host_Name,
+                 Public_User_Identification,
+                 Loglevel,
+                 PBX_Secret,
+                 PBX_Loglevel,
+                 PBX_Host,
+                 PBX_Port,
+                 User_Backend_Type,
+                 User_Map_File,
+                 Auth_Server,
+                 Contact_Server,
+                 Access_Log,
+                 Error_Log,
+                 Server_Token);
+
+   type Defaults_Array is array (Keys) of Unbounded_String;
+
+   Default_Values : constant Defaults_Array :=
+     (Auth_Server
+      => U ("http://localhost:8080"),
+      Contact_Server
+      => U ("http://localhost:4010"),
+      Loglevel
+      => U ("Information"),
+      Access_Log
+      => U ("access.log"),
+      Error_Log
+      => U ("error.log"),
+      Cache_Max_Element_Age
+      => U ("86_400"),
+      Host_Name
+      => U ("please_update_main_conf"),
+      Public_User_Identification
+      => U ("FALSE"),
+      PBX_Secret
+      => U ("1234"),
+      PBX_Loglevel
+      => U ("Information"),
+      PBX_Host
+      => U ("localhost"),
+      PBX_Port
+      => U ("8021"),
+      User_Backend_Type
+      => U ("file"),
+      User_Map_File
+      => U ("static_json/agent.list"),
+      Server_Token
+      => Ada.Strings.Unbounded.Null_Unbounded_String);
+
+end Configuration.Default;
