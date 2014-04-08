056b3569c0143f3c95b346e3ec9dbab806cc4763
Jacob Sparre Andersen <jsa@adaheads.com>
2014-04-08 14:01:29 +0000

    Current name-spaces for Call-Flow-Control.  (See also issue #9.)

--- src/namespaces.adb
+++ src/namespaces.adb
@@ -1,42 +1,65 @@
-package Namespaces is
-   function Name (Item  : in String;
-                  Index : in Positive) return String;
+with Ada.Strings.Fixed;
 
-   function Name (Item  : in String;
-                  Index : in Positive) return String is
+package body Namespaces is
+   function Name (Item : in String;
+                  Head : in String := "/") return String;
+
+   function Call_Resource (Item : in Black.Request.Instance)
+                          return Call_Resources is
+      URI : constant String := Item.Resource;
+   begin
+      return Call_Resources'Value (Name (Item => URI,
+                                         Head => "/call/"));
+   exception
+      when others =>
+         return Not_Found;
+   end Call_Resource;
+
+   function Name (Item : in String;
+                  Head : in String := "/") return String is
+      use Ada.Strings;
       From      : Positive;
       Separator : Natural := Item'First;
    begin
-      for Counter in 1 .. Index loop
-         First := Separator;
+      if Head = Fixed.Head (Item, Head'Length) then
+         From := Item'First + Head'Length;
 
-         if Item (First) = '/' then
-            Separator := Index (Source  => Item (First + 1 .. Item'Last),
-                                Pattern => "/");
+         Separator := Fixed.Index (Source  => Item (From .. Item'Last),
+                                   Pattern => "/");
 
-            if Separator = 0 then
-               Separator := Index (Source  => Item (First + 1 .. Item'Last),
-                                   Pattern => "?");
-            end if;
-
-            if Separator = 0 then
-               Separator = Index'Last + 1;
-            end if;
-         else
-            raise Black.Protocol_Error
-              with "Resource should start with '/'.";
+         if Separator = 0 then
+            Separator := Fixed.Index (Source  => Item (From .. Item'Last),
+                                      Pattern => "?");
          end if;
-      end loop;
 
-      return Item (From + 1 .. Separator - 1);
+         if Separator = 0 then
+            Separator := Item'Last + 1;
+         end if;
+
+         return Item (From .. Separator - 1);
+      else
+         return "";
+      end if;
    end Name;
 
    function Resource (Item : in Black.Request.Instance) return Resources is
       URI : constant String := Item.Resource;
    begin
-      return Resources'Value (Name (URI, 1));
+      return Resources'Value (Name (Item => URI,
+                                    Head => "/"));
    exception
       when others =>
          return Not_Found;
    end Resource;
+
+   function Users_Resource (Item : in Black.Request.Instance)
+                           return Users_Resources is
+      URI : constant String := Item.Resource;
+   begin
+      return Users_Resources'Value (Name (Item => URI,
+                                          Head => "/users/"));
+   exception
+      when others =>
+         return Not_Found;
+   end Users_Resource;
 end Namespaces;
--- src/namespaces.ads
+++ src/namespaces.ads
@@ -1,7 +1,15 @@
 with Black.Request;
 
 package Namespaces is
-   type Resources is (Not_Found, Call, User);
+   type Resources       is (Not_Found, Call, Users);
+   type Call_Resources  is (Not_Found, Hangup, List, Originate, Park, Pickup,
+                            Queue, Transfer);
+   type Users_Resources is (Not_Found, List, Pause);
 
-   function Resource (Item : in Black.Request.Instance) return Resources;
+   function Resource (Item : in Black.Request.Instance)
+                     return Resources;
+   function Call_Resource (Item : in Black.Request.Instance)
+                          return Call_Resources;
+   function Users_Resource (Item : in Black.Request.Instance)
+                           return Users_Resources;
 end Namespaces;
