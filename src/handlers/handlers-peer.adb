09c1cc724bda11e7ecae62536e5c71a29d59d1b8
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-20 11:02:26 +0200

    Request handlers for plain-socket peer interface.

---
+++ src/handlers/handlers-peer.adb
@@ -0,0 +1,27 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                     Copyright (C) 2014-, AdaHeads K/S                     --
+--                                                                           --
+--  This is free software;  you can redistribute it and/or modify it         --
+--  under terms of the  GNU General Public License  as published by the      --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any  --
+--  later version. This library is distributed in the hope that it will be   --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--  You should have received a copy of the GNU General Public License and    --
+--  a copy of the GCC Runtime Library Exception along with this program;     --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+package body Handlers.Peer is
+
+   function List_Handler return Response.Instance is
+   begin
+      return Response.Create
+        (Status      => Response.Internal_Error,
+         Description => "Not implemented");
+   end List_Handler;
+
+end Handlers.Peer;
